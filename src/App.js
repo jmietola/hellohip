ee0f9502f04f53ac8817776ad6e057c24021a00e
Jouni Mietola <jouni.mietola@gmail.com>
2017-09-29 15:53:16 +0300

    Add fetch  and location info

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {users: []}
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        fetch(`/users?lat=${position.coords.latitude}&lng=${position.coords.longitude}`)
+        .then(response => {
+            response.text().then(text => {
+              this.setState({
+                 status: text
+              })
+           });
+         })
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        })
+
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,9 +39,15 @@ class App extends Component {
         <p className="App-intro">
           Meethip
         </p>
+        <p>Latitude: {this.state.latitude}</p>
+        <p>Longitude: {this.state.longitude}</p>
+        {this.state.error ? <p>Error: {this.state.error}</p> : null}
+        <p> {this.state.status}</p>
       </div>
     );
   }
 }
+
+
 
 export default App;
