e225eef545128d17bbe77a460afd1bc4de066591
Jouni Mietola <jouni.mietola@gmail.com>
2017-10-01 22:55:38 +0300

    Fix absolute url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const  socket = openSocket('http://localhost:3000');
+const  socket = openSocket('http://:3000');
 function subscribeToTimer(cb) {
   socket.on('timer', timestamp => cb(null, timestamp));
   socket.emit('subscribeToTimer', 1000);
