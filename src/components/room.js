c3c4c059a9dde3c15a458d0a910981023f4fc23c
Jouni Mietola <jouni.mietola@gmail.com>
2017-10-02 10:28:04 +0300

    Add input and submit for a chat

--- src/components/room.js
+++ src/components/room.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './room.css';
 const io = require('socket.io-client')
 const socket = io()
 
 class Room extends React.Component {
   constructor(props) {
     super(props)
+    this.handleSubmit = this.handleSubmit.bind(this);
     socket.on('add user', (user) => this.addUser(user));
   }
 
@@ -12,11 +14,23 @@ class Room extends React.Component {
     // Add User
   }
 
+  handleSubmit (event) {
+    console.log("handle submit");
+  var socket = io();
+  event.preventDefault();
+  socket.emit('chat message', document.getElementById('m').value);
+  document.getElementById('m').value = '';
+
+  }
+
   render() {
     return (
       <div>
         <h1>Chat Room</h1>
-
+        <ul id="messages"></ul>
+        <form action="" onSubmit={this.handleSubmit}>
+          <input id="m" autocomplete="off" /><button>Send</button>
+        </form>
       </div>
 
     )
